5b2052da7ef420acd3123ea4f89f83f84c1416ed
frutifiel <gastonbustamante99@gmail.com>
2024-09-13 21:01:30 -0300

    primer archivo subido

---
+++ proyecto.py
@@ -0,0 +1,38 @@
+print('hola mundo')
+variable ='florencia itati pizzio' 
+variable_2 =26
+variable_3 ='Paraguay' #cadena
+variable_4 ='B' #caracter
+variable_5 = 1.45
+print('el nombre de la colo es',variable)
+print('la edad de la colo es',variable_2)
+print('la colo es de',variable_3)
+print('la colo iba al curso',variable_4)
+print('la colo mide',variable_5)
+
+
+print(variable_3[1])
+print(variable_3[3])
+print(variable_3[6])
+
+veces = 5
+resultado = variable_3 * 5
+print(resultado)
+
+print ("fekers fekers\nfrutifiel alan lionel alberto emiliano ramirez")
+
+print('holaaaaa \nenteeeeee')
+print('t\tpython\t\tess genial')
+print('Juan\' "Perezz')
+print("karla\"Gomez")
+print('Caracter \\ diagonal \'harina\'')
+
+variable_convertir = str(variable_2)
+
+concatenacion = variable_convertir + ' ' + variable_3
+print(concatenacion)
+
+variable_convertir2 = int(variable_convertir)
+print(type(variable_convertir))
+print(type(variable_2))
+print(type(variable_5))
